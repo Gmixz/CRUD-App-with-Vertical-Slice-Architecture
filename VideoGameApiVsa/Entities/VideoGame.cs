431e7ab5a4a2d0ac0e9da7c3142d2878f0d09a1a
Gmixz <ros.biomed@gmail.com>
2025-04-17 14:15:07 -0400

    Add project files.

---
+++ VideoGameApiVsa/Entities/VideoGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace VideoGameApiVsa.Entities
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+    }
+}
